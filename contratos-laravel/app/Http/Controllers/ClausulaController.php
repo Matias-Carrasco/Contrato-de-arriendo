97121b9e99f398f5a81d461001a1e29d6001b597
Matias Carrasco <matfcvega@gmail.com>
2021-04-26 13:57:38 -0400

    Cambios a los controladores

--- contratos-laravel/app/Http/Controllers/ClausulaController.php
+++ contratos-laravel/app/Http/Controllers/ClausulaController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Clausula;
 use Illuminate\Http\Request;
 
 class ClausulaController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Clausula  $clausula
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Clausula $clausula)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Clausula  $clausula
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Clausula $clausula)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Clausula  $clausula
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Clausula $clausula)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Clausula  $clausula
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Clausula $clausula)
+    {
+        //
+    }
 }
--- contratos-laravel/app/Http/Controllers/RepresentanteProvController.php
+++ contratos-laravel/app/Http/Controllers/RepresentanteProvController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Representante_prov;
 use Illuminate\Http\Request;
 
 class RepresentanteProvController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Representante_prov  $representante_prov
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Representante_prov $representante_prov)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Representante_prov  $representante_prov
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Representante_prov $representante_prov)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Representante_prov  $representante_prov
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Representante_prov $representante_prov)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Representante_prov  $representante_prov
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Representante_prov $representante_prov)
+    {
+        //
+    }
 }
