c11f806ed05e24dedec5f3a4abfbbc30e20a8922
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2021-05-01 18:02:46 -0400

    sidebar fix

--- contratos-laravel/resources/views/includes/sidebar.blade.php
+++ contratos-laravel/resources/views/includes/sidebar.blade.php
@@ -2,43 +2,93 @@
     <div class="logo">
         <h4 class="text-light font-weight-bold mb-0">INGENIERÍA DE SOFTWARE</h4>
     </div>
-    <div class="menu">
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
-            Inicio</a>
+    <ul class="list-unstyled components">
+        <p>Dummy Heading</p>
+       
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
+                Inicio</a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-users"></i>
+                Usuarios</a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Arriendos</a>
+        </li>
+        <li class="active">
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Contratos</a>
+            <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                    <a href="/proveedor" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                        Proveedor</a>
+                </li>
+                <li>
+                    <a href="/representante_prov" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                        Representante Proveedor</a>
+                </li>
+                <li>
+                    <a href="/clausula" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                        Clausular</a>
+                </li>
+                <li>
+                    <a href="/anexo" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                        Anexor</a>
+                </li>
+            </ul>
+        </li>
+       
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Despachos </a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Asignaciones</a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Trabajos</a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Garantías</a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Seguros </a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Solicitudes </a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-chart-line"></i>
+                Estadísticas</a>
+        </li>
+        <li>
+            <a href="../perfil/" class="d-block text-light p-3 border-0"><i class="fas fa-user"></i>
+                Perfil</a>
+        </li>
+        <li>
+            <a href="../perfil/" class="d-block text-light p-3 border-0"><i class="fas fa-user"></i>
+                Perfil</a>
+        </li>
+        <li>
+            <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-sliders-h"></i>
+                Configuración</a>
+        </li>
+        
 
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-users"></i>
-            Usuarios</a>
 
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-            Arriendos</a>
 
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-        Contratos</a>
 
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-        Despachos </a>
 
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-        Asignaciones</a>
+       
+    </ul>
 
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-        Trabajos</a>
-
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-        Garantías</a>
-
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-        Seguros </a>
-
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-        Solicitudes </a>
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-chart-line"></i>
-            Estadísticas</a>
-
-        <a href="../perfil/" class="d-block text-light p-3 border-0"><i class="fas fa-user"></i>
-            Perfil</a>
-
-        <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-sliders-h"></i>
-            Configuración</a>
-    </div>
+</nav>
+   
 </div>
