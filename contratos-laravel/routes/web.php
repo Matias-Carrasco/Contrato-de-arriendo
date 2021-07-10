705ba48a26ae4148198d693f1bc73024546c484a
Mauricio Lobos <mauri.loboss@gmail.com>
2021-07-10 16:20:17 -0400

    cambios en web.php

--- contratos-laravel/routes/web.php
+++ contratos-laravel/routes/web.php
@@ -21,13 +21,8 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('representante_prov', '\App\Http\Controllers\RepresentanteProvController');
-//Route::resource('proveedor','\App\Http\Controllers\ProveedorController');
+Route::get('proveedor/ID_ciudad','\App\Http\Controllers\ProveedorController@ID_ciudad');
+Route::resource('proveedor','\App\Http\Controllers\ProveedorController');
 Route::resource('contrato', '\App\Http\Controllers\ContratoController');
 Route::resource('anexo', '\App\Http\Controllers\AnexoController');
 Route::resource('clausula', '\App\Http\Controllers\ClausulaController');
-Route::get('proveedor','\App\Http\Controllers\ProveedorController@index');
-Route::get('proveedor/create','\App\Http\Controllers\ProveedorController@create');
-Route::post('proveedor','\App\Http\Controllers\ProveedorController@store');
-Route::get('proveedor/ID_ciudad','\App\Http\Controllers\ProveedorController@ID_ciudad');
-Route::get('proveedor/{proveedor}/edit','\App\Http\Controllers\ProveedorController@edit');
-Route::put('proveedor/{proveedor}','\App\Http\Controllers\ProveedorController@update');
