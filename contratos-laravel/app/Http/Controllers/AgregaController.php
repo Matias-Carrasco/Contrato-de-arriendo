59e9f3d555372b6c0f2da5afb3b407bb156b7805
BasCandia <bcandia.0123@gmail.com>
2021-07-10 15:43:26 -0400

    fix a controlador y modelos de relaciones

--- contratos-laravel/app/Http/Controllers/AgregaController.php
+++ contratos-laravel/app/Http/Controllers/AgregaController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Agrega;
 use Illuminate\Http\Request;
 
 class AgregaController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Agrega  $agrega
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Agrega $agrega)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Agrega  $agrega
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Agrega $agrega)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Agrega  $agrega
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Agrega $agrega)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Agrega  $agrega
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Agrega $agrega)
+    {
+        //
+    }
 }
