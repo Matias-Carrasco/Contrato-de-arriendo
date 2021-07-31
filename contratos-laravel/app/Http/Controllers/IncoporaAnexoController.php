bc120e932d1a527bd89d6fb05969220f2b238897
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2021-07-31 15:49:06 -0400

    Incorpora_anexo

---
+++ contratos-laravel/app/Http/Controllers/IncoporaAnexoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Incopora_anexo;
+use Illuminate\Http\Request;
+
+class IncoporaAnexoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Incopora_anexo  $incopora_anexo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Incopora_anexo $incopora_anexo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Incopora_anexo  $incopora_anexo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Incopora_anexo $incopora_anexo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Incopora_anexo  $incopora_anexo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Incopora_anexo $incopora_anexo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Incopora_anexo  $incopora_anexo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Incopora_anexo $incopora_anexo)
+    {
+        //
+    }
+}
---
+++ contratos-laravel/database/migrations/2021_07_31_193356_create_incopora_anexos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncoporaAnexosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incopora_anexos', function (Blueprint $table) {
+            $table->bigInteger('ID_anexo')->unsigned();
+            $table->foreign('ID_anexo')->references('ID_contrato')->on('contratos');
+            $table->bigInteger('ID_perfil')->unsigned();
+            $table->foreign('ID_perfil')->references('ID_perfil')->on('perfils');
+            $table->integer('Cantidad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incopora_anexos');
+    }
+}
