8a22ec38200f42477c5d8612e39f4442b9478d0a
Mauricio Lobos <mauri.loboss@gmail.com>
2021-08-01 20:40:28 -0400

    cambios 01 pdf anexo

---
+++ contratos-laravel/resources/views/PDF/PdfAnexo.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PRUEBA DE LIBRERIA</title>
+</head>
+<body>
+    Anexo del contrato {{$anexo[0]->ID_contrato}}
+    <br>
+    Anexo en estado  {{$anexo[0]->ID_estado}}
+    
+
+
+</body>
+</html>
