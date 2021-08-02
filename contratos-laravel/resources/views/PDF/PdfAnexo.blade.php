aacc424ea37b1a6c58cd93fb9ba6ce1916a5d1f6
Mauricio Lobos <mauri.loboss@gmail.com>
2021-08-01 20:57:26 -0400

    PDF anexo 03

--- contratos-laravel/resources/views/PDF/PdfAnexo.blade.php
+++ contratos-laravel/resources/views/PDF/PdfAnexo.blade.php
@@ -8,7 +8,53 @@
     Anexo del contrato {{$anexo[0]->ID_contrato}}
     <br>
     Anexo en estado  {{$anexo[0]->ID_estado}}
+
+    <p>
+        PERFILES
+        <br>
+        @foreach($incorporas as $incorpora)
+            @foreach($perfiles as $perfil)
+                
+            
+                @if($incorpora->ID_perfil == $perfil->ID_perfil)
+                    Cantidad:  {{$incorpora->Cantidad}}
+                    <br>
+                    Nombre perfil: {{$perfil->Nombre_perfil}}
+                    <br>
+                    Procesador: {{$perfil->Tipo_procesador}}
+                    <br>
+                    RAM: {{$perfil->Ram}}
+                    <br>
+                    Lector de dvd: {{$perfil->Lector_dvd}}
+                    <br>
+                    Tarjeta de sonido: {{$perfil->Tarjeta_sonido}}
+                    <br>
+                    Tarjeta de video: {{$perfil->Tarjeta_video}}
+                    <br>
+                    Tarjeta de red: {{$perfil->Tarjeta_red}}
+                    <br>
+                    Teclado: {{$perfil->Teclado}}
+                    <br>
+                    Gabinete: {{$perfil->Gabinete}}
+                    <br>
+                    Mouse: {{$perfil->Mouse}}
+                    <br>
+                    Fuente de poder: {{$perfil->Fuente_de_poder}}
+                    <br>
+                    SSD: {{$perfil->SSD}}
+                    <br>
+                    Bajo impacto acustico: {{$perfil->Bajo_impacto_acustico}}
+                    <br>
+                    Valor perfil: {{$perfil->Valor_perfil}}
+                    <br>
+                @endif
+                {{$incorpora->Cambios_a_clausula}}
+            
     
+            @endforeach
+        
+        @endforeach
+    </p>
 
 
 </body>
